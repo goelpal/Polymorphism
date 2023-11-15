5089f2d2543a686124beed8d9c59399c6a402079
goelpal <pallavisingha@gmail.com>
2023-11-15 08:43:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Polymorphism examples
+
+Console.WriteLine("Polymorphism");
+
+Car car = new Car();   
+Bicycle bicycle = new Bicycle();
+Boat boat = new Boat();
+
+//Car[] vehicle = { car, bicycle, boat };// We cannot add other types of objects to car array
+
+Vehicle[] vehicle = { car, bicycle, boat };
+
+foreach (Vehicle v in vehicle)
+{
+    v.Go();
+}
+
+Console.ReadLine();
+
+class Vehicle 
+{
+    public virtual void Go()
+    {
+        Console.WriteLine("The vehicle is moving");
+    }
+}
+
+class Car : Vehicle 
+{
+    public override void Go()
+    {
+        Console.WriteLine("The car is moving");
+    }
+}
+
+class Bicycle : Vehicle 
+{
+    public override void Go()
+    {
+        Console.WriteLine("The Bicycle is moving");
+    }
+} 
+
+class Boat : Vehicle 
+{
+    public override void Go()
+    {
+        Console.WriteLine("The boat is moving");
+    }
+}    
